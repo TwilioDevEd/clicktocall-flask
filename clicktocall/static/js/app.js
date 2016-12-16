205b599463f6e49e73fbb1ef16d69702e87e6276
José Oliveros <jose.oliveros.1983@gmail.com>
2016-12-16 11:57:58 -0600

    Update js client to recieve Sales Number

--- clicktocall/static/js/app.js
+++ clicktocall/static/js/app.js
@@ -1,29 +1,30 @@
 // Execute JavaScript on page load
 $(function() {
     // Initialize phone number text input plugin
-    $('#phoneNumber').intlTelInput({
+    $('#phoneNumber, #salesNumber').intlTelInput({
         responsiveDropdown: true,
         autoFormat: true,
-        utilsScript: '/static/js/libphonenumber/build/utils.js'
+        utilsScript: '/static/js/libphonenumber/src/utils.js'
     });
 
     // Intercept form submission and submit the form with ajax
     $('#contactForm').on('submit', function(e) {
-        // Prevent submit event from bubbling and automatically 
+        // Prevent submit event from bubbling and automatically
         // submitting the form
         e.preventDefault();
 
-        // Call our ajax endpoint on the server to initialize the 
+        // Call our ajax endpoint on the server to initialize the
         // phone call
         $.ajax({
             url: '/call',
             method: 'POST',
             dataType: 'json',
             data: {
-                phoneNumber: $('#phoneNumber').val()
+              phoneNumber: $('#phoneNumber').val(),
+              salesNumber: $('#salesNumber').val(),
             }
         }).done(function(data) {
-            // The JSON sent back from the server will contain 
+            // The JSON sent back from the server will contain
             // a success message
             alert(data.message);
         }).fail(function(error) {
